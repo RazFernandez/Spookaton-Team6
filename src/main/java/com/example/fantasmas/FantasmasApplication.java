38f4b955daa23c20c7ce3ad1029250f8ebe650f3
RazFernandez <mr.fernandez.mx@outlook.com>
2025-10-31 16:09:32 -0600

    Added new post

--- src/main/java/com/example/fantasmas/FantasmasApplication.java
+++ src/main/java/com/example/fantasmas/FantasmasApplication.java
@@ -2,12 +2,15 @@ package com.example.fantasmas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.fantasmas.UI.ScapeRoomGame;
 
 @SpringBootApplication
 public class FantasmasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FantasmasApplication.class, args);
+		ScapeRoomGame game = new ScapeRoomGame();
+		game.startGame();
 	}
 
 }
